bd9a666e0a6af0f6d043888c233556a4536b8a31
rabbitism <popmessiah@126.com>
2018-05-30 13:30:07 +0800

    CSharp Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpAlgorithmPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
