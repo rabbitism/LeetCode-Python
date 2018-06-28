3c3da62412610094995d0a60207f4a087d0e6ebc
rabbitism <popmessiah@126.com>
2018-06-28 16:33:18 +0800

    Update 404

---
+++ CSharp/Solutions/404.cs
@@ -0,0 +1,33 @@
+/*
+Find the sum of all left leaves in a given binary tree.
+
+Example:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+
+using CSharp;
+namespace CSharp{
+    public partial class Solution {
+        public int SumOfLeftLeaves(TreeNode root) {
+            return 0;
+        }
+    }
+}
+
